7622d779ea35d2e0c2007dbbb89df799a86e728a
Philip Weinke <philip.weinke@digitalnoise.de>
2021-12-02 07:56:22 +0100

    Solution for 2021-02

---
+++ src/Year2021/Day02/Solver.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace AdventOfCode\Year2021\Day02;
+
+use AdventOfCode\Common\Input;
+
+final class Solver
+{
+    private const REGEX = '/^(.+) (\d+)$/';
+
+    public function partOne(Input $input): int
+    {
+        $instructions = $input->matchLines(self::REGEX);
+
+        $position = 0;
+        $depth = 0;
+
+        foreach ($instructions as $instruction) {
+            match ($instruction[0]) {
+                'forward' => $position += (int)$instruction[1],
+                'down' => $depth += (int)$instruction[1],
+                'up' => $depth -= (int)$instruction[1],
+            };
+        }
+
+        return $position * $depth;
+    }
+
+    public function partTwo(Input $input): int
+    {
+        $instructions = $input->matchLines(self::REGEX);
+
+        $position = 0;
+        $depth = 0;
+        $aim = 0;
+
+        foreach ($instructions as $instruction) {
+            switch ($instruction[0]) {
+                case 'forward':
+                    $position += (int)$instruction[1];
+                    $depth += $aim * (int)$instruction[1];
+                    break;
+
+                case 'down':
+                    $aim += (int)$instruction[1];
+                    break;
+
+                case 'up':
+                    $aim -= (int)$instruction[1];
+                    break;
+            }
+        }
+
+        return $position * $depth;
+    }
+}
---
+++ src/Year2021/Day02/SolverTest.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace AdventOfCode\Year2021\Day02;
+
+use AdventOfCode\Common\Input;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \AdventOfCode\Year2021\Day02\Solver
+ */
+final class SolverTest extends TestCase
+{
+    private Solver $subject;
+
+    /**
+     * @test
+     */
+    public function partOne(): void
+    {
+        self::assertSame(
+            150,
+            $this->subject->partOne(
+                Input::fromArray([
+                    'forward 5',
+                    'down 5',
+                    'forward 8',
+                    'up 3',
+                    'down 8',
+                    'forward 2',
+                ])
+            )
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function partTwo(): void
+    {
+        self::assertSame(
+            900,
+            $this->subject->partTwo(
+                Input::fromArray([
+                    'forward 5',
+                    'down 5',
+                    'forward 8',
+                    'up 3',
+                    'down 8',
+                    'forward 2',
+                ])
+            )
+        );
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = new Solver();
+    }
+}
