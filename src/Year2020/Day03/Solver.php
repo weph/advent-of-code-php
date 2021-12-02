617be51e0e73e4d4285047243e3cb06781ce4a1e
Philip Weinke <philip.weinke@digitalnoise.de>
2021-12-02 15:25:56 +0100

    Solution for 2020-03

---
+++ src/Year2020/Day03/Solver.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace AdventOfCode\Year2020\Day03;
+
+use AdventOfCode\Common\Input;
+use function count;
+use function strlen;
+
+final class Solver
+{
+    public function partOne(Input $input): int
+    {
+        return $this->countTrees($input->lines(), 3, 1);
+    }
+
+    private function countTrees(array $input, int $mx, int $my): int
+    {
+        $rows = count($input);
+        $trees = 0;
+        $x = 0;
+        $y = 0;
+
+        while ($y < $rows) {
+            $row = $input[$y];
+
+            $trees += (int)($row[$x % strlen($row)] === '#');
+
+            $x += $mx;
+            $y += $my;
+        }
+
+        return $trees;
+    }
+
+    public function partTwo(Input $input): int
+    {
+        $map = $input->lines();
+
+        return $this->countTrees($map, 1, 1) *
+            $this->countTrees($map, 3, 1) *
+            $this->countTrees($map, 5, 1) *
+            $this->countTrees($map, 7, 1) *
+            $this->countTrees($map, 1, 2);
+    }
+}
---
+++ src/Year2020/Day03/SolverTest.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace AdventOfCode\Year2020\Day03;
+
+use AdventOfCode\Common\Input;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \AdventOfCode\Year2020\Day03\Solver
+ */
+final class SolverTest extends TestCase
+{
+    private const EXAMPLE_INPUT = [
+        '..##.......',
+        '#...#...#..',
+        '.#....#..#.',
+        '..#.#...#.#',
+        '.#...##..#.',
+        '..#.##.....',
+        '.#.#.#....#',
+        '.#........#',
+        '#.##...#...',
+        '#...##....#',
+        '.#..#...#.#',
+    ];
+
+    private Solver $subject;
+
+    /**
+     * @test
+     */
+    public function partOne(): void
+    {
+        self::assertSame(7, $this->subject->partOne(Input::fromArray(self::EXAMPLE_INPUT)));
+    }
+
+    /**
+     * @test
+     */
+    public function partTwo(): void
+    {
+        self::assertSame(336, $this->subject->partTwo(Input::fromArray(self::EXAMPLE_INPUT)));
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = new Solver();
+    }
+}
