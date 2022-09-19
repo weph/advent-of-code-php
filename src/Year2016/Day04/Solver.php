85becc1b5a65583b9c3afe39541e7cf470121c31
Philip Weinke <philip.weinke@digitalnoise.de>
2022-09-19 07:49:04 +0200

    Solution for 2016-04

---
+++ src/Year2016/Day04/Solver.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace AdventOfCode\Year2016\Day04;
+
+use AdventOfCode\Common\Input;
+use AdventOfCode\Common\PuzzleSolver;
+use function array_sum;
+
+final class Solver implements PuzzleSolver
+{
+    public function partOne(Input $input): int
+    {
+        return (int)array_sum(
+            $input->matchLines('/(.+)-(\d+)\[(.+)\]/')
+                ->filter(static function (array $matches) {
+                    $counts = count_chars(str_replace('-', '', $matches[0]), 1);
+                    arsort($counts);
+                    $checksum = implode(array_map('\chr', array_slice(array_keys($counts), 0, 5)));
+
+                    return $checksum === $matches[2];
+                })
+                ->map(static fn(array $matches) => $matches[1])
+                ->asArray()
+        );
+    }
+
+    /**
+     * @psalm-pure
+     */
+    private static function rotate(string $s, int $amount): string
+    {
+        return implode(
+            array_map(
+                static fn(string $c) => chr(((ord($c) - ord('a') + $amount) % 26) + ord('a')),
+                str_split($s)
+            )
+        );
+    }
+
+    public function partTwo(Input $input): int
+    {
+        $locations = $input->matchLines('/(.+)-(\d+)\[(.+)\]/')
+            ->filter(static function (array $matches) {
+                $name = implode(' ', array_map(static fn(string $s) => self::rotate($s, (int)$matches[1]), explode('-', $matches[0])));
+
+                return $name === 'northpole object storage';
+            })
+            ->map(static fn(array $matches) => $matches[1])
+            ->asArray();
+
+        return (int)$locations[0];
+    }
+}
---
+++ src/Year2016/Day04/SolverTest.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace AdventOfCode\Year2016\Day04;
+
+use AdventOfCode\Common\Input;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \AdventOfCode\Year2016\Day04\Solver
+ */
+final class SolverTest extends TestCase
+{
+    private Solver $subject;
+
+    /**
+     * @test
+     */
+    public function partOne(): void
+    {
+        $input = Input::fromArray(
+            [
+                'aaaaa-bbb-z-y-x-123[abxyz]',
+                'a-b-c-d-e-f-g-h-987[abcde]',
+                'not-a-real-room-404[oarel]',
+                'totally-real-room-200[decoy]',
+            ]
+        );
+
+        self::assertSame(1514, $this->subject->partOne($input));
+    }
+
+    /**
+     * @test
+     */
+    public function partTwo(): void
+    {
+        $input = Input::fromArray(
+            [
+                'qzmt-zixmtkozy-ivhz-343[xxxxx]',
+                'rsvxltspi-sfnigx-wxsveki-984[sixve]'
+            ]
+        );
+
+        self::assertSame(984, $this->subject->partTwo($input));
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = new Solver();
+    }
+}
