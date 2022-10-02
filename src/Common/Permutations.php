608d981eabbb1860473d2901c1f6820c324c2b06
Philip Weinke <philip.weinke@digitalnoise.de>
2022-10-02 17:34:32 +0200

    Add implementation of Heap's algorithm to generate permutations

---
+++ src/Common/Permutations.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace AdventOfCode\Common;
+
+final class Permutations
+{
+    /**
+     * @template T
+     * @param list<T> $values
+     * @return list<list<T>>
+     */
+    public static function of(array $values): array
+    {
+        return self::heapPermutations($values, count($values));
+    }
+
+    /**
+     * @template T
+     * @param array<T> $values
+     * @return list<list<T>>
+     *
+     * @see https://en.wikipedia.org/wiki/Heap%27s_algorithm
+     */
+    private static function heapPermutations(array &$values, int $size): array
+    {
+        if ($size === 1) {
+            return [array_values($values)];
+        }
+
+        $result = [];
+
+        for ($i = 0; $i < $size; $i++) {
+            $result[] = self::heapPermutations($values, $size - 1);
+
+            if ($size % 2 === 0) {
+                [$values[$i], $values[$size - 1]] = [$values[$size - 1], $values[$i]];
+            } else {
+                [$values[$size - 1], $values[0]] = [$values[0], $values[$size - 1]];
+            }
+        }
+
+        return array_merge(...$result);
+    }
+}
---
+++ src/Common/PermutationsTest.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+
+namespace AdventOfCode\Common;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \AdventOfCode\Common\Permutations
+ */
+final class PermutationsTest extends TestCase
+{
+    /**
+     * @param list<string> $expected
+     *
+     * @test
+     * @dataProvider examples
+     */
+    public function permutations(string $input, array $expected): void
+    {
+        $values = explode(',', $input);
+
+        $result = Permutations::of($values);
+
+        self::assertEquals($expected, array_map(static fn(array $v) => implode(',', $v), $result));
+    }
+
+    /**
+     * @return iterable<array-key, array{0: string, 1: list<string>}>
+     */
+    public function examples(): iterable
+    {
+        yield [
+            'a',
+            [
+                'a'
+            ]
+        ];
+
+        yield [
+            'a,b',
+            [
+                'a,b',
+                'b,a'
+            ]
+        ];
+
+        yield [
+            'a,b,c',
+            [
+                'a,b,c',
+                'b,a,c',
+                'c,a,b',
+                'a,c,b',
+                'b,c,a',
+                'c,b,a'
+            ]
+        ];
+
+        yield [
+            'a,b,c,d',
+            [
+                'a,b,c,d',
+                'b,a,c,d',
+                'c,a,b,d',
+                'a,c,b,d',
+                'b,c,a,d',
+                'c,b,a,d',
+
+                'd,b,c,a',
+                'b,d,c,a',
+                'c,d,b,a',
+                'd,c,b,a',
+                'b,c,d,a',
+                'c,b,d,a',
+
+                'd,a,c,b',
+                'a,d,c,b',
+                'c,d,a,b',
+                'd,c,a,b',
+                'a,c,d,b',
+                'c,a,d,b',
+
+                'd,a,b,c',
+                'a,d,b,c',
+                'b,d,a,c',
+                'd,b,a,c',
+                'a,b,d,c',
+                'b,a,d,c',
+            ]
+        ];
+    }
+}
