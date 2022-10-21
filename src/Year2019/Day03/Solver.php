da8441f876327f49569a8a389962c67970ebe018
Philip Weinke <philip.weinke@digitalnoise.de>
2022-10-21 07:38:55 +0200

    Solution for 2019-03

---
+++ src/Year2019/Day03/Solver.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types=1);
+
+namespace AdventOfCode\Year2019\Day03;
+
+use AdventOfCode\Common\Input;
+use AdventOfCode\Common\Line;
+use AdventOfCode\Common\Point;
+use AdventOfCode\Common\PuzzleSolver;
+use RuntimeException;
+
+final class Solver implements PuzzleSolver
+{
+    public function partOne(Input $input): int
+    {
+        $grid = $this->grid($input);
+
+        return min(
+            array_map(
+                static fn(string $s) => abs(Point::fromString($s)->x) + abs(Point::fromString($s)->y),
+                array_keys($this->intersections($grid))
+            )
+        );
+    }
+
+    public function partTwo(Input $input): int
+    {
+        return (int)min(array_map(array_sum(...), $this->intersections($this->grid($input))));
+    }
+
+    /**
+     * @param Input $input
+     * @return array<string, array<int, int>>
+     */
+    private function grid(Input $input): array
+    {
+        $grid = [];
+
+        foreach ($input->lines()->asArray() as $wire => $line) {
+            preg_match_all('/([RLUD])(\d+),?/', $line, $matches, PREG_SET_ORDER);
+
+            $current = new Point(0, 0);
+            $steps = 0;
+            foreach ($matches as $match) {
+                $direction = $match[1];
+                $distance = (int)$match[2];
+
+                $next = match ($direction) {
+                    'R' => $current->addX($distance),
+                    'L' => $current->addX(-$distance),
+                    'U' => $current->addY($distance),
+                    'D' => $current->addY(-$distance),
+                };
+
+                foreach (array_slice((new Line($current, $next))->points(), 1) as $point) {
+                    $grid[$point->asString()][$wire] = ++$steps;
+                }
+
+                $current = $next;
+            }
+        }
+
+        return $grid;
+    }
+
+    /**
+     * @param array<string, array<int, int>> $grid
+     * @return non-empty-array<string, array<int, int>>
+     */
+    private function intersections(array $grid): array
+    {
+        $intersections = array_filter($grid, static fn(array $v) => count($v) > 1);
+
+        if ($intersections === []) {
+            throw new RuntimeException('No intersections found');
+        }
+
+        return $intersections;
+    }
+}
---
+++ src/Year2019/Day03/SolverTest.php
@@ -0,0 +1,104 @@
+<?php
+declare(strict_types=1);
+
+namespace AdventOfCode\Year2019\Day03;
+
+use AdventOfCode\Common\Input;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \AdventOfCode\Year2019\Day03\Solver
+ */
+final class SolverTest extends TestCase
+{
+    private Solver $subject;
+
+    /**
+     * @param list<string> $input
+     *
+     * @test
+     * @dataProvider partOneExamples
+     */
+    public function partOne(array $input, int $expected): void
+    {
+        self::assertEquals($expected, $this->subject->partOne(Input::fromArray($input)));
+    }
+
+    /**
+     * @return iterable<array-key, array{0: list<string>, 1: int}>
+     */
+    public function partOneExamples(): iterable
+    {
+        yield [
+            [
+                'R8,U5,L5,D3',
+                'U7,R6,D4,L4',
+            ],
+            6
+        ];
+
+        yield [
+            [
+                'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+                'U62,R66,U55,R34,D71,R55,D58,R83',
+            ],
+            159
+        ];
+
+        yield [
+            [
+                'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+                'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
+            ],
+            135
+        ];
+    }
+
+    /**
+     * @param list<string> $input
+     *
+     * @test
+     * @dataProvider partTwoExamples
+     */
+    public function partTwo(array $input, int $expected): void
+    {
+        self::assertEquals($expected, $this->subject->partTwo(Input::fromArray($input)));
+    }
+
+    /**
+     * @return iterable<array-key, array{0: list<string>, 1: int}>
+     */
+    public function partTwoExamples(): iterable
+    {
+        yield [
+            [
+                'R8,U5,L5,D3',
+                'U7,R6,D4,L4',
+            ],
+            30
+        ];
+
+        yield [
+            [
+                'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+                'U62,R66,U55,R34,D71,R55,D58,R83',
+            ],
+            610
+        ];
+
+        yield [
+            [
+                'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+                'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
+            ],
+            410
+        ];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = new Solver();
+    }
+}
